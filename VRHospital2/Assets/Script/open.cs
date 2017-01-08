940e8da024c8b2cc9124173d0667c89833b7f3ea
JF <15828589290@163.com>
2017-01-08 22:43:53 +0800

    newProject

---
+++ VRHospital2/Assets/Script/open.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class open : MonoBehaviour {
+    SteamVR_Controller.Device device;
+    private float rotationSpeed;
+    public GameObject target;
+    public GameObject center;
+    private float distance;
+    private bool triggerpress;
+	// Use this for initialization
+	void Start () {
+        rotationSpeed = 0.5f;
+        device = SteamVR_Controller.Input((int)GetComponent<SteamVR_TrackedObject>().index);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        triggerpress = device.GetPress(SteamVR_Controller.ButtonMask.Trigger);
+        distance = Vector3.Distance(target.transform.position, transform.position);
+        if (distance <= 0.2f&&triggerpress)
+        {
+            if (target.transform.localRotation.y >= -90f && target.transform.localRotation.y <= 80.0f)
+            {
+                center.transform.Rotate(new Vector3(0, rotationSpeed, 0));
+            }
+        }
+	}
+}
---
+++ VRHospital2/Assets/Script/showdetail.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class showdetail : MonoBehaviour {
+    public GameObject detail;
+    public GameObject target;
+    public AudioClip record;
+    AudioSource recordSound;
+    public SteamVR_Controller.Device device;
+    private bool triggerpress;
+    private bool play;
+    private float distance;
+    private float timer;
+	// Use this for initialization
+	void Start () {
+        device = SteamVR_Controller.Input((int)GetComponent<SteamVR_TrackedObject>().index);
+        recordSound = gameObject.AddComponent<AudioSource>();
+        recordSound.clip = record;
+        play = true;
+        timer = 3f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        triggerpress = device.GetPress(SteamVR_Controller.ButtonMask.Trigger);
+        distance= Vector3.Distance(target.transform.position, transform.position);
+        if (distance<=0.15f&&triggerpress)
+        {
+            timer -= Time.deltaTime;
+            detail.SetActive(true);
+            if (timer <= 0&&play)
+            {
+                recordSound.Play();
+                play = false;
+                timer = 3f;
+            }
+        }
+        else
+        {
+            detail.SetActive(false);
+            timer = 3f;
+            play = true;
+        }
+	}
+}
